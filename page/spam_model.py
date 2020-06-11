7da43bc7a4ad79af8f4eb16d6be7218c0833da4d
VaishnaviBhardwaj <vaishnavibhardwaj27@gmail.com>
2020-06-11 17:25:48 +0530

    First commit

---
+++ page/spam_model.py
@@ -0,0 +1,66 @@
+
+# coding: utf-8
+
+# In[2]:import numpy as np # linear algebra
+import pandas as pd 
+import os
+df = pd.read_csv(r"C:\Users\Vaishnavi Bhardwaj\Desktop\data SET\spam_ham_dataset.csv")
+df.head()
+
+
+# In[3]:
+
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(df["text"],df["label"], test_size = 0.1, random_state = 10)
+from sklearn.feature_extraction.text import CountVectorizer
+vect = CountVectorizer()
+vect.fit(X_train)
+X_train_df = vect.transform(X_train)
+X_test_df = vect.transform(X_test)
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+model = RandomForestClassifier()
+model.fit(X_train_df,y_train)
+
+target = model.predict(X_test_df)
+accuracy_score(y_test,target)
+
+
+# In[4]:
+
+
+from joblib import dump, load
+dump(model, 'model.joblib')
+dump(vect, 'vector.joblib')
+
+
+# In[5]:
+
+
+model = load('model.joblib')
+vect = load('vector.joblib')
+
+
+# In[6]:
+
+
+def is_spam(inp = ["FREE FREE FREE FREE"]):
+    if model.predict(vect.transform(inp))[0] == "spam":
+        return "It is a Spam "
+    else:
+        return "It is a Ham"
+
+
+# In[8]:
+
+
+print(is_spam(inp = ["free"]))
+
+
+# In[9]:
+
+
+print(is_spam(inp = ["""Congratulations You have won 10000$. FREE FREE FREE .Come and collect.
+"""]))
+
---
+++ page/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .spam_model import is_spam
+from django.contrib import messages
+# Create your views here.
+# from sklearn.externals import joblib
+# reloadModel =joblib.load('static/spam_model.ipynb')
+
+def Homepage(request):
+    return render(request,"home.html")
+
+
+
+
+
+@csrf_exempt
+def SpamModel(request):
+	#  form = ProductForm(request.POST or None)
+	# if form.is_valid():
+	# 	save_it = form.save(commit=False)
+	# 	save_it.save()
+ #    # print(request)
+    # print("chlgya")
+    # print(request.POST.dict())
+    if request.method == "POST":
+        temp={}
+        temp['message']=request.POST.get('message')
+        content={
+        "msg":(is_spam(inp = [temp['message']]))  
+        }
+
+    return render(request,"SpamModel.html",content)
+
+
+
+
+ #    form = ProductForm(request.POST or None)
+	# if form.is_valid():
+	# 	save_it = form.save(commit=False)
+	# 	save_it.save()
+	# 	messages.success(request, ' Thank You For Your Reaponse !!!!!')
